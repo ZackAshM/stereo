Updates and Fixes on Rotation Implementation.

examples/run_sim.py
@@ -45,6 +45,7 @@ def run_sim(
         output: str = None,
         star_min: int = 4,
         snr: float = None,
+        rotation: float = None,
         verbose_factor: int = 100,
         single_run: bool = False,
         save_image: bool = False,
@@ -66,6 +67,9 @@ def run_sim(
         Skips the trial if the generated field has less than this many stars.
     snr : float
         If given, forces the image to the desired signal-to-noise ratio.
+    rotation : float
+        The rotation in arcsec/s. If None, rotation is selected randomly from
+        a Gaussian range in ANITA flightpath data.
     verbose_factor : int
         Prints the progress every this many trials.
     single_run : bool
@@ -181,10 +185,13 @@ def run_sim(
                 result_ra = -999
                 result_dec = -999
                 solve_t = -999
+                data_snr = -999
+                data_rot = -999
             
                 data_line = [[
                     trials, solved, alt_err, ind_val, num_stars, 
-                    real_ra, real_dec, result_ra, result_dec, solve_t
+                    real_ra, real_dec, result_ra, result_dec, solve_t,
+                    data_snr, data_rot
                 ]]
                 data_generator = itertools.chain(data_generator, data_line)
                 
@@ -197,7 +204,8 @@ def run_sim(
                 img.writeto(filename, overwrite=True)
 
             # make the image
-            image.snr = snr if snr else image.snr
+            image.rotation = rotation if rotation else np.random.normal(loc=152.504, scale=949.721)
+            image.snr = snr
             make_image = image.image
 
             # plot image
@@ -228,10 +236,13 @@ def run_sim(
                 result_ra = -999
                 result_dec = -999
                 solve_t = -999
+                data_snr = image.snr
+                data_rot = image.rotation
             
                 data_line = [[
                     trials, solved, alt_err, ind_val, num_stars, 
-                    real_ra, real_dec, result_ra, result_dec, solve_t
+                    real_ra, real_dec, result_ra, result_dec, solve_t,
+                    data_snr, data_rot
                 ]]
                 data_generator = itertools.chain(data_generator, data_line)
 
@@ -261,10 +272,13 @@ def run_sim(
                 result_ra = -999
                 result_dec = -999
                 solve_t = solve_tf
+                data_snr = image.snr
+                data_rot = image.rotation
             
                 data_line = [[
                     trials, solved, alt_err, ind_val, num_stars, 
-                    real_ra, real_dec, result_ra, result_dec, solve_t
+                    real_ra, real_dec, result_ra, result_dec, solve_t,
+                    data_snr, data_rot
                 ]]
                 data_generator = itertools.chain(data_generator, data_line)
 
@@ -293,10 +307,13 @@ def run_sim(
                 result_ra = result.ra.value
                 result_dec = result.dec.value
                 solve_t = solve_tf
+                data_snr = image.snr
+                data_rot = image.rotation
             
                 data_line = [[
                     trials, solved, alt_err, ind_val, num_stars, 
-                    real_ra, real_dec, result_ra, result_dec, solve_t
+                    real_ra, real_dec, result_ra, result_dec, solve_t,
+                    data_snr, data_rot
                 ]]
                 data_generator = itertools.chain(data_generator, data_line)
 
@@ -325,8 +342,7 @@ def run_sim(
             timeout_fail_per_solver = 100 * timeout_fail / (trial_num - star_num_fail)
             timeout_fail_per_total = 100 * timeout_fail / trial_num
             star_num_fail_per_total = 100 * star_num_fail / trial_num
-            snr_str = '# SNR: {0}\n'.format(snr) if snr is not None else ''
-            skip_str = '# skip_header = 24\n' if snr is not None else '# skip_header = 23\n'
+            skip_str = '# skip_header = 23\n'
 
             out.write(
                 '# STEREO Data for run titled "{0}"\n'.format(run_name) + 
@@ -340,7 +356,7 @@ def run_sim(
                 '# Low Star Counts (<{0}): {1}, {2}%\n'.format(
                     star_min, star_num_fail, star_num_fail_per_total) +
                 '# Total Data Collection Time: {0} hrs\n'.format(total_t) + 
-                snr_str + skip_str + 
+                skip_str + 
                 '#\n# Column Notes\n' + 
                 '# num: Data Index\n' + 
                 '# solved: 1 = Success, 0 = Fail, -1 = Undetermined (low star count or timed out)\n' + 
@@ -352,12 +368,14 @@ def run_sim(
                 '# ra: Result RA of the center of the field in degrees\n' + 
                 '# dec: Result DEC of the center of the field in degrees\n' +
                 '# solve_time: Total time taken to solve (or fail) in seconds\n' +
-                '#\n# num     solved     alt_error     A4_index     star_ct     real_ra     real_dec     ra     dec     solve_time\n'
+                '# snr: mean(signal) / sqrt(mean(background))\n' +
+                '# rotation: payload/camera rotation in arcsec/s\n' +
+                '#\n# num     solved     alt_error     A4_index     star_ct     real_ra     real_dec     ra     dec     solve_time     snr     rotation\n'
             )
 
             for data in data_generator:
                 out.write(
-                    '{0}     {1}     {2}     {3}     {4}     {5}     {6}     {7}     {8}     {9}\n'.format(*data)
+                    '{0}     {1}     {2}     {3}     {4}     {5}     {6}     {7}     {8}     {9}     {10}     {11}\n'.format(*data)
                 )
 
             out.close()
@@ -387,8 +405,7 @@ def run_sim(
             timeout_fail_per_solver = 100 * timeout_fail / (trial_num - star_num_fail)
             timeout_fail_per_total = 100 * timeout_fail / trial_num
             star_num_fail_per_total = 100 * star_num_fail / trial_num
-            snr_str = '# SNR: {0}\n'.format(snr) if snr is not None else ''
-            skip_str = '# skip_header = 24\n' if snr is not None else '# skip_header = 23\n'
+            skip_str = '# skip_header = 23\n'
 
             out.write(
                 '# STEREO Data for run titled "{0}"\n'.format(run_name) + 
@@ -402,7 +419,7 @@ def run_sim(
                 '# Low Star Counts (<{0}): {1}, {2}%\n'.format(
                     star_min, star_num_fail, star_num_fail_per_total) +
                 '# Total Data Collection Time: {0} hrs\n'.format(total_t) + 
-                snr_str + skip_str + 
+                skip_str + 
                 '#\n# Column Notes\n' + 
                 '# num: Data Index\n' + 
                 '# solved: 1 = Success, 0 = Fail, -1 = Undetermined (low star count or timed out)\n' + 
@@ -414,12 +431,14 @@ def run_sim(
                 '# ra: Result RA of the center of the field in degrees\n' + 
                 '# dec: Result DEC of the center of the field in degrees\n' +
                 '# solve_time: Total time taken to solve (or fail) in seconds\n' +
-                '#\n# num     solved     alt_error     A4_index     star_ct     real_ra     real_dec     ra     dec     solve_time\n'
+                '# snr: mean(signal) / sqrt(mean(background))\n' +
+                '# rotation: payload/camera rotation in arcsec/s\n' +
+                '#\n# num     solved     alt_error     A4_index     star_ct     real_ra     real_dec     ra     dec     solve_time     snr     rotation\n'
             )
-            
+
             for data in data_generator:
                 out.write(
-                    '{0}     {1}     {2}     {3}     {4}     {5}     {6}     {7}     {8}     {9}\n'.format(*data)
+                    '{0}     {1}     {2}     {3}     {4}     {5}     {6}     {7}     {8}     {9}     {10}     {11}\n'.format(*data)
                 )
 
             out.close()
@@ -438,6 +457,8 @@ parser.add_argument('-t', '--TOTAL_SOLVE_TRIALS', type=float, default=100,
                     help='int; number of solved trials to accomplish; default=100')
 parser.add_argument('--SNR', type=float, default=None,
                     help='float; the desired signal-to-noise ratio; default=None')
+parser.add_argument('--ROTATION', type=float, default=None,
+                    help='float; the desired rotation of the payload')
 parser.add_argument('-v', '--VERBOSE_FACTOR', type=int, default=100,
                     help='int; print progress per this factor; default=100')
 parser.add_argument('-s', '--SINGLE_RUN', action='store_true',
@@ -452,6 +473,7 @@ args = parser.parse_args()
 TST = args.TOTAL_SOLVE_TRIALS
 RN = args.RUN_NAME
 SNR = args.SNR
+ROT = args.ROTATION
 VF = args.VERBOSE_FACTOR
 SR = args.SINGLE_RUN
 SI = args.SAVE_IMAGE
@@ -463,6 +485,7 @@ run_sim(
     RN,
     star_min=4,
     snr=SNR,
+    rotation=ROT,
     verbose_factor=VF,
     single_run=SR,
     save_image=SI,

stereo/sim/image.py
@@ -2,7 +2,7 @@
 This module provides the Image class.
 """
 
-from typing import Dict, Tuple, Any
+from typing import Any, Dict, Tuple
 
 import attr
 import numpy as np
@@ -43,7 +43,7 @@ class Image(object):
     psf_sigma : float
         The standard deviation of the Gaussian PSF in pixels. Default = 3.
     snr : float
-        If specified, forces the image to have this signal-to-noise ratio 
+        If specified, forces the image to have this signal-to-noise ratio
         defined by mean(star count) / sqrt(mean(noise count)).
     rotation : float
         The camera rotation in arcsec/sec.
@@ -116,14 +116,16 @@ class Image(object):
     def trail_length(self) -> int:
         """Returns the trail length of stars in the image in pixels."""
         try:
-            return self._trail_length # type: ignore
+            return self._trail_length  # type: ignore
         except AttributeError:
             if self.cam is None:
                 raise ValueError("Camera object required to determine trail length")
-            trail_length = int(self.rotation * self.cam.exp_time / self.cam.pixscale)
+            trail_length = abs(
+                int(self.rotation * self.cam.exp_time / self.cam.pixscale)
+            )
             trail_length += 1
             self._trail_length = trail_length
-            return trail_length            
+            return trail_length
 
     @property
     def altaz_stars(self) -> Table:
@@ -318,12 +320,14 @@ class Image(object):
                 dark_current_level * self.cam.exp_time, size=self.size
             )
             background = read_noise + dark_current
-            
+
             # force signal-to-noise ratio if desired
+            signal = np.mean(image_data[image_data > 0])
+            noise = np.mean(background[background > 0]) ** 0.5
             if self.snr is not None:
-                signal = np.mean(image_data[image_data > 0])
-                noise = np.mean(background[background > 0])**0.5
                 image_data *= self.snr / (signal / noise)
+            else:
+                self.snr = signal / noise
 
             # add background
             image_data += background
@@ -448,7 +452,7 @@ class Image(object):
         Parameters
         ----------
         vmin, vmax : float
-            The min and max passed into the pyplot LogNorm scale. 
+            The min and max passed into the pyplot LogNorm scale.
             Default = image min/max.
         centroids : ndarray
             The centroids of the image. If not None, returns the image with position
@@ -462,18 +466,32 @@ class Image(object):
             Keyword arguments passed to plt.subplots
         """
 
-        if 'figsize' not in subplots_kws:
-            subplots_kws['figsize'] = [20,10]
+        if "figsize" not in subplots_kws:
+            subplots_kws["figsize"] = [20, 10]
         fig, ax = plt.subplots(**subplots_kws)
         plt.gray()
 
         if self.snr:
-            title = r'ra{0:.2f}$\degree$dec{1:.2f}$\degree$, FoV: {2:.1f}$\degree$, SNR: {3:.1f}, $\omega$: {4:0}"/s'.format(
-                self.center.ra.value, self.center.dec.value, self.cam.fov[2], self.snr, self.rotation
+            title = (
+                r"ra{0:.2f}$\degree$dec{1:.2f}$\degree$, "
+                + r"FoV: {2:.1f}$\degree$, SNR: {3:.1f}, $\omega$: "
+                + r'{4:0}"/s'
+            ).format(
+                self.center.ra.value,
+                self.center.dec.value,
+                self.cam.fov[2],
+                self.snr,
+                self.rotation,
             )
         else:
-            title = r'ra{0:.2f}$\degree$dec{1:.2f}$\degree$, FoV: {2:.1f}$\degree$, $\omega$: {3:0}"/s'.format(
-                self.center.ra.value, self.center.dec.value, self.cam.fov[2], self.rotation
+            title = (
+                r"ra{0:.2f}$\degree$dec{1:.2f}$\degree$, "
+                + r'FoV: {2:.1f}$\degree$, $\omega$: {3:0}"/s'
+            ).format(
+                self.center.ra.value,
+                self.center.dec.value,
+                self.cam.fov[2],
+                self.rotation,
             )
         plt.title(title)
         vmin = np.min(self.image) if vmin is None else vmin
@@ -491,6 +509,6 @@ class Image(object):
 
         # save if desired
         if filename:
-            plt.savefig(filename + ".png", bbox_inches='tight', pad_inches=0)
+            plt.savefig(filename + ".png", bbox_inches="tight", pad_inches=0)
 
         plt.show()